af4aae59fc15a9e8fe45647a92351e3898a1a5b3
EthanSpicher <ethan.spicher@emu.edu>
2023-02-15 07:43:22 -0500

    Update robotic-agriculture.py
    Here is where I added the fuction that should be able to control the servo motor. Caleb if you could look over this and see what you think that would be great!

--- robotic-agriculture.py
+++ robotic-agriculture.py
@@ -1,3 +1,18 @@
 from machine import Pin
 
 ledPin = Pin()
+
+
+
+
+# I wasnt sure where I should add the code to cntrol the servo so I put it here. 
+# It appears to be a very simple code with the ESP32.
+#Referance Website https://icircuit.net/micropython-controlling-servo-esp32-nodemcu/2385
+
+import machine
+
+def servo_motor():
+    p4 = machine.Pin(4) #assignes the pin 4 output to the servo motor.
+    servo = machine.PWM(p4, freq = 50) #calls the pin and the frequency to send to the pin.
+    servo.duty(10) #Tells the servo motor to go to said degree (10 degrees).
+    servo.duty(80) #Tells servo to move to other said degree (80 degreees).
