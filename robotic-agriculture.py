7d6e0aab3f95b5d0065c053cec623dcfc9921463
Caleb Hostetler <18chostetler@gmail.com>
2023-02-18 14:19:05 -0500

    Define all pins

--- robotic-agriculture.py
+++ robotic-agriculture.py
@@ -1,18 +1,36 @@
-from machine import Pin
-
-ledPin = Pin()
-
-
-
-
-# I wasnt sure where I should add the code to cntrol the servo so I put it here. 
-# It appears to be a very simple code with the ESP32.
-#Referance Website https://icircuit.net/micropython-controlling-servo-esp32-nodemcu/2385
 
 import machine
+from machine import Pin
 
+# Define all of the pins
+dir_pin = Pin(19, Pin.OUT)
+x1_step_pin = Pin(13, Pin.OUT)
+x2_step_pin = Pin(16, Pin.OUT)
+y_step_pin = Pin(17, Pin.OUT)
+z_step_pin = Pin(18, Pin.OUT)
+
+valve_pin = Pin(22, Pin.OUT)
+servo_pin = Pin(21, Pin.OUT)
+
+flow_pin = Pin(23, Pin.IN)
+moisture1_pin = Pin(34, Pin.IN)
+moisture1_pin = Pin(39, Pin.IN)
+moisture1_pin = Pin(36, Pin.IN)
+
+xp_lim_pin = Pin(4, Pin.IN, Pin.PULL_UP)
+xn_lim_pin = Pin(26, Pin.IN, Pin.PULL_UP)
+yp_lim_pin = Pin(25, Pin.IN, Pin.PULL_UP)
+yn_lim_pin = Pin(33, Pin.IN, Pin.PULL_UP)
+zp_lim_pin = Pin(32, Pin.IN, Pin.PULL_UP)
+zn_lim_pin = Pin(35, Pin.IN, Pin.PULL_UP)
+
+
+
+
+
+#Reference: https://icircuit.net/micropython-controlling-servo-esp32-nodemcu/2385
 def servo_motor():
-    p4 = machine.Pin(4) #assignes the pin 4 output to the servo motor.
+    p4 = machine.Pin(4) 
     servo = machine.PWM(p4, freq = 50) #calls the pin and the frequency to send to the pin.
     servo.duty(10) #Tells the servo motor to go to said degree (10 degrees).
     servo.duty(80) #Tells servo to move to other said degree (80 degreees).
