0c9cc66c2fe1b43c351017d1135847fea4cea899
Caleb Hostetler <18chostetler@gmail.com>
2023-02-23 10:40:28 -0500

    Move duplicated motor functionality to new Motor class

--- robotic-agriculture.py
+++ robotic-agriculture.py
@@ -53,57 +53,76 @@ class MotorSystem:
     Y = 1
     Z = 2
     def __init__(self):
-        '''Initialize the motors'''
+        self.x_motor = Motor(x1_step_pin, dir_pin)
+        self.y_motor = Motor(x1_step_pin, dir_pin)
+        self.z_motor = Motor(x1_step_pin, dir_pin)
+        pass
+class Motor:
+    def __init__(self, step_pin, dir_pin):
+        '''Initialize the motor'''
         self.min_pulse_width = 3 # Minimum width of pulse in microseconds. Minimum for DRV8825 is 1.9us, 3 gives us some wiggle room
-        self.set_position(0, 0, 0)
-        self.set_velocity(0, 0, 0)
-        self.set_target(0, 0, 0)
-        self.x_previous_step_ticks = 0
-        self.y_previous_step_ticks = 0
-        self.z_previous_step_ticks = 0
-        self.set_pins(x1_step_pin, y_step_pin, z_step_pin, dir_pin)
-
-    def set_target(self, x, y, z):
-        '''Set the target position that the gantry will go to in the future'''
-        self.x_tar = x
-        self.y_tar = y
-        self.z_tar = z
-
-    def set_position(self, x, y, z):
-        '''Set the current position of the system. Note that this is *not* the target position'''
-        self.x_pos = x
-        self.y_pos = y
-        self.z_pos = z
-
-    def set_velocity(self, xv, yv, zv):
-        '''Set the current velocities in steps/sec'''
-        self.x_vel = xv
-        self.y_vel = yv
-        self.z_vel = zv
-        # Set the interval between steps in microseconds 
-        self.x_step_interval = (math.inf if xv==0 else int(1_000_000 * 1/xv))
-        self.y_step_interval = (math.inf if yv==0 else int(1_000_000 * 1/yv))
-        self.z_step_interval = (math.inf if zv==0 else int(1_000_000 * 1/zv))
-
-    def set_max_velocity(self, max_velocity, direction=None):
-        '''Set the maximum velocity in a given direction (0=X, 1=Y, 2=Z, None=All directions). Only needs to be called once.'''
-        if direction in (MotorSystem.X, None): self.max_x_vel = max_velocity
-        if direction in (MotorSystem.Y, None): self.max_y_vel = max_velocity
-        if direction in (MotorSystem.Z, None): self.max_z_vel = max_velocity
-
-    def set_max_acceleration(self, max_acceleration, direction=None):
-        '''Set the maximum velocity in a given direction (0=X, 1=Y, 2=Z, None=All directions)'''
-        if direction in (MotorSystem.X, None): self.max_x_accel = max_acceleration
-        if direction in (MotorSystem.Y, None): self.max_y_accel = max_acceleration
-        if direction in (MotorSystem.Z, None): self.max_z_accel = max_acceleration
+        self.set_position(0)
+        self.set_velocity(0)
+        self.set_target(0)
+        self.previous_step_ticks = 0
+        self.set_pin(step_pin, dir_pin)
         pass
 
-    def set_pins(self, x, y, z, dir):
-        '''Set the pins to be used for stepping the motors.'''
-        self.x_pin = x
-        self.y_pin = y
-        self.z_pin = z
+    def set_pin(self, step, dir):
+        '''Set the pins to be used for stepping the motor.'''
+        self.step_pin = step
         self.dir_pin = dir
+
+    def update(self):
+        '''Should be called every clock cycle. This function will pulse the stepper motor needed to move the target position'''
+        self.update_vel()
+
+    def update_vel(self):
+        '''Is called by update(). Changes the stepper velocity at the rate of `self.max_accel`. This function makes sure that we don't accelerate or decelerate too quickly'''
+        pass
+
+    def set_target(self, tar):
+        '''Set the target position that the motor will go to in the future'''
+        self.target = tar
+
+    def set_position(self, pos):
+        '''Set the current position of the motor. Note that this is *not* the target position'''
+        self.position = pos
+
+    def set_velocity(self, v):
+        '''Set the current velocity in steps/sec'''
+        self.velocity = v
+        # Set the interval between steps in microseconds 
+        self.step_interval = (math.inf if v==0 else int(1_000_000 * 1/v))
+
+    def set_max_velocity(self, max_velocity):
+        '''Set the maximum velocity. Only needs to be called once.'''
+        self.max_vel = max_velocity
+
+    def set_max_acceleration(self, max_acceleration):
+        '''Set the maximum velocity in a given direction (0=X, 1=Y, 2=Z, None=All directions)'''
+        self.max_accel = max_acceleration
+
+    def run_speed(self):
+        '''Checks the timers for the motor and does a `step()` if needed.'''
+        current_time = time.ticks_us()
+        # print(current_time)
+        if time.ticks_diff(current_time, self.previous_step_ticks) >= self.step_interval:
+            self.step(False) # Todo: add functionality for reverse here
+            self.previous_step_ticks = time.ticks_add(self.previous_step_ticks, self.step_interval)
+
+    def step(self, reverse=False):
+        '''Perform one step. If `reverse` is set, go the opposite way. Uses `self.min_pulse_width` to calculate length of pulse'''
+        # First, set the direction pin
+        if (reverse): 
+            self.dir_pin.on()
+        else:
+            self.dir_pin.off()
+
+        # Then, pulse the desired pin as quickly as possible
+        self.step_pin.on()
+        time.sleep_us(self.min_pulse_width)
+        self.step_pin.off()
 
     def distance_to_steps(self, distance, direction):
         '''Converts a distance in m to a number of steps in a given direction (X=0, Y=1, Z=2). See also `steps_to_distance`. '''
@@ -116,49 +135,7 @@ class MotorSystem:
     def calibrate(self):
         '''calibration subroutine - not sure how this will work'''
         pass
-
-    def update(self):
-        '''Should be called every clock cycle. This function will pulse the stepper motors needed to move the target position'''
-        self.update_vel()
-
-    def update_vel(self):
-        '''Is called by update(). Changes the stepper velocities at the rate of `self.max_accel`. This function makes sure that we don't accelerate or decelerate too quickly'''
-        pass
-
-    def step(self, direction, reverse=False):
-        '''Perform one step in the given direction. If `reverse` is set, go the opposite way. Uses `self.min_pulse_width` to calculate length of pulse'''
-        # First, set the direction pin
-        if (reverse): 
-            self.dir_pin.on()
-        else:
-            self.dir_pin.off()
-
-        # Then, pulse the desired pin as quickly as possible
-        pulse_pin = None
-        if direction == MotorSystem.X: pulse_pin = self.x_pin
-        elif direction == MotorSystem.Y: pulse_pin = self.y_pin
-        elif direction == MotorSystem.Z: pulse_pin = self.z_pin
-        if pulse_pin:
-            pulse_pin.on()
-            time.sleep_us(self.min_pulse_width)
-            pulse_pin.off()
-
-    def run_speed(self):
-        '''Checks the timers for each motor and does a `step()` if needed.'''
-        current_time = time.ticks_us()
-        # print(current_time)
-        if time.ticks_diff(current_time, self.x_previous_step_ticks) >= self.x_step_interval:
-            self.step(MotorSystem.X, False) # Todo: add functionality for reverse here
-            self.x_previous_step_ticks = time.ticks_add(self.x_previous_step_ticks, self.x_step_interval)
-        if time.ticks_diff(current_time, self.y_previous_step_ticks) >= self.y_step_interval:
-            self.step(MotorSystem.Y, False) # Todo: add functionality for reverse here
-            self.y_previous_step_ticks = time.ticks_add(self.y_previous_step_ticks, self.y_step_interval)
-        if time.ticks_diff(current_time, self.z_previous_step_ticks) >= self.z_step_interval:
-            self.step(MotorSystem.Z, False) # Todo: add functionality for reverse here
-            self.z_previous_step_ticks = time.ticks_add(self.z_previous_step_ticks, self.z_step_interval)
-
-
-
+        
 #Reference: https://icircuit.net/micropython-controlling-servo-esp32-nodemcu/2385
 def set_servo_pos(duty):
     '''Limits are 27 (dispense) and 65 (collect)'''
