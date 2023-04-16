fb1e9a511ab3cb982162f44b5e28a42a6fd1d1cd
Caleb Hostetler <18chostetler@gmail.com>
2023-04-16 08:32:41 -0400

    MQTT connection added

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mqtt import Mqtt
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'
@@ -9,7 +10,8 @@ app.config['MQTT_KEEPALIVE'] = 5
 
 mqtt = Mqtt(app)
 
-last_update = None
+bot_data = dict()
+bot_data['last_update'] = None
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
@@ -21,13 +23,20 @@ def handle_mqtt_message(client, userdata, message):
         topic=message.topic,
         payload=message.payload.decode()
     )
-    data = json.loads(data['payload'])
+    bot_data.update(json.loads(data['payload']))
+    bot_data['last_update'] = datetime.now().strftime("%d %b. %H:%M:%S")
     print(data)
 
 # Route to display the latest message
 @app.route('/')
 def index():
-    return render_template('index.html', latest_message='hello world')
+    return render_template('index.html', data=bot_data)
+
+@app.route('/publish', methods=['POST'])
+def publish_message():
+    payload = request.form['payload']
+    mqtt.publish('emuagrobot22802/control', payload)
+    return 'Message published'
 
 if __name__ == '__main__':
     app.run(debug=True)
